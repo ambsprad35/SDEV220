15ba3780a2af6ca361bcf7d4909a9f01c420cd7a
ambsprad35 <aspradley2@ivytech.edu>
2025-01-26 21:19:22 -0500

    added validation checking

--- StudentGPA.py
+++ StudentGPA.py
@@ -12,17 +12,21 @@ float gpa - student's GPA
 """
 
 # Prompt the user for the student's last name 'ZZZ' quits the app
-lName = input("Enter the student's last name or ZZZ to quit.")
+lName = input("Enter the student's last name or ZZZ to quit.").upper()
 
 while(lName != "ZZZ"): # Only continue if 'ZZZ' was not entered
     fName = input("Enter the student's first name.")
-    gpa = float(input("Enter the student's GPA."))
-
+    try:
+        gpa = float(input("Enter the student's GPA."))
+    except ValueError:
+        print("GPA must be a valid number")
+        break
+    
     if(gpa >= 3.5): #Determine if the student is either on the Dean's list or Honor Roll
         print("Congrats! " + fName + " " + lName + " has made the Dean's List.")
     elif (3.25 <= gpa < 3.5):
         print("Congrats! " + fName + " " + lName + " has made the Honor Roll.")
     else:
-        print("Sorry. " + fName + " " + lName + " did not make either list.")
-        
+        print("Sorry. " + fName + " " + lName + " did not make either list.") 
+    
     lName = input("Enter the student's last name or ZZZ to quit.")
